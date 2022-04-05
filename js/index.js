962fe54f4a0102bf9689c1c9d3b00207087d0cb0
KalenkinA <KalenkinAI@mail.ru>
2022-04-05 19:19:09 +0300

    second commit

---
+++ js/index.js
@@ -0,0 +1,28 @@
+//const name1 = 'vasyan';
+//console.log(name1);
+
+// type number
+let age = 19;
+console.log(typeof age);
+
+// type string
+//let name = "Vanya";
+//let name = 'Vanya';
+let name = `Vanya is ${age} years old`;
+console.log(typeof typeof name, name);
+
+// type boolean
+let isDay = true;
+console.log(typeof isDay);
+
+// type undefined
+let someData1;
+console.log(typeof someData1);
+
+// type object (of null)
+let someData2 = null;
+console.log(typeof someData2);
+
+// type object
+let user = {name: 'Alex', age: 25};
+console.log(typeof user, user);
