3e545f691674e0f97815ab8dc56b56244b359766
KalenkinA <KalenkinAI@mail.ru>
2022-04-20 09:05:09 +0300

    циклы и условия

---
+++ js/examples/logical_operators.js
@@ -0,0 +1,16 @@
+// OR
+if(1 || 0) {
+    console.log("success");
+}
+
+// AND
+if(1 && 0) {
+    console.log("success");
+}
+
+// NOT
+if(!0) {
+    console.log("success");
+}
+
+// приоритеты по убыванию NOT - AND - OR
